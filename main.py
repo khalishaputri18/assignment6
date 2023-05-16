aabb14b0e197bab2da43c78574a832f9ac439207
khalishaputri18 <khalishaputri18@gmail.com>
2023-05-16 13:50:29 +0700

    Added multiplication and division feature

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    
+    elif operator == "*":
+        result = mul(data_1,data_2)
+    
+    elif operator == "/":
+        result = div(data_1, data_2)
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
